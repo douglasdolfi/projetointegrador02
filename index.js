020dad5cf9bea3d5f38b4c86ad57d868ca5071ab
Douglas Dolfi <85909200+douglasdolfi@users.noreply.github.com>
2024-11-06 18:04:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT}`);
 });
 
-
+// Rota para a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 // Rota POST para receber dados do formulário
 app.post('/cadastrar', (req, res) => {
