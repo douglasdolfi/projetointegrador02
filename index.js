6074b99a0ea56d8a911fe941fcc76f322da22798
Douglas Dolfi <85909200+douglasdolfi@users.noreply.github.com>
2024-11-06 18:12:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // index.js
-import express from 'express'
+import express from 'express';
 
-const express = require('express')
-const app = express()
-const PORT = 4000
+const express = require('express');
+const app = express();
+const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT}`);
