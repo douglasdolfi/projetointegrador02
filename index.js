ce356b95052b1115258bc929ed30aef0ffbfb254
Douglas Dolfi <85909200+douglasdolfi@users.noreply.github.com>
2024-11-06 17:53:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,40 @@ app.listen(PORT, () => {
 app.get('/',(req,res) => {
 	res.send('Nossa API está funcionando!')
 })
+
+// Configura o Express para ler dados do formulário
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Servir arquivos estáticos da pasta "public"
+app.use(express.static('public'));
+
+// Rota para a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Rota POST para receber dados do formulário
+app.post('/cadastrar', (req, res) => {
+  const { nome, idade, endereco, ministerios, quantidade_filhos, nome_filhos, sugestoes } = req.body;
+
+  // Exibe os dados no console
+  console.log("Dados recebidos do formulário:");
+  console.log("Nome:", nome);
+  console.log("Idade:", idade);
+  console.log("Endereço:", endereco);
+  console.log("Ministérios:", ministerios);
+  console.log("Quantidade de Filhos:", quantidade_filhos);
+  console.log("Nome dos Filhos:", nome_filhos);
+  console.log("Sugestões:", sugestoes);
+
+  // Enviar uma resposta para o navegador
+  res.send('Cadastro recebido com sucesso!');
+
+
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em PORT ${port}`);
+});
