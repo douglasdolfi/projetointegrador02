097c96d868ef3aa41239558dd3ca5b709ed81382
Douglas Dolfi <85909200+douglasdolfi@users.noreply.github.com>
2024-11-06 17:57:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ app.listen(PORT, () => {
 
 app.get('/',(req,res) => {
 	res.send('Nossa API está funcionando!')
-	// res.sendFile(__dirname + '/public/index.html')
-});
+})
 
 // Configura o Express para ler dados do formulário
 app.use(express.urlencoded({ extended: true }));
